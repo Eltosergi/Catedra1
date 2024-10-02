f12c8ffb8e844970c32a088325b521461cb4624a
Sergio Parada <sergio.parada@alumnos.ucn.cl>
2024-10-02 20:00:34 -0300

    Add models

---
+++ src/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catedra_1.src.Models
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Rut { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty ;
+        public string Gender { get; set; } = string.Empty;
+        public string BirthDay { get; set; } = string.Empty;
+        
+    }
+}
